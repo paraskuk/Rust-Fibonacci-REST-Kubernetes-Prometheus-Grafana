c1053acc9cd22ce7e09c4916d2cefc0907217a5e
Paraskevas Paraskevas <paraskevasp@gmail.com>
2024-12-24 19:23:07 +0000

    add doctests

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -2,7 +2,23 @@ pub mod measure_time;
 
 use std::collections::HashMap;
 
+///
+///
+/// # Arguments
+///
+/// * `n`:  u32, the nth number in the fibonacci sequence
+///
+/// returns: u32,the result of the nth number in the fibonacci sequence
+///
+/// # Examples
+///
+/// ```
+///  use fibonacci::fibonacci;
+/// let result = fibonacci(1);
+///  assert_eq!(result, 1);
+/// ```
 pub fn fibonacci(n: u32) -> u32 {
+
     if n < 2 {
         n
     } else {
@@ -10,6 +26,22 @@ pub fn fibonacci(n: u32) -> u32 {
     }
 }
 
+
+///
+///
+/// # Arguments
+///
+/// * `n`:  u32, the nth number in the fibonacci sequence
+///
+/// returns: u32, result of the nth number in the fibonacci sequence
+///
+/// # Examples
+///
+/// ```
+/// use fibonacci::fibonacci_match;
+///  let result = fibonacci_match(3);
+///  assert_eq!(result, 2);
+/// ```
 pub fn fibonacci_match(n: u32) -> u32 {
     match n {
         0 => 0,
@@ -18,6 +50,23 @@ pub fn fibonacci_match(n: u32) -> u32 {
     }
 }
 
+
+///
+///
+/// # Arguments
+///
+/// * `n`:  u32, the nth number in the fibonacci sequence
+///
+/// returns: u32  the result of the nth number in the fibonacci sequence
+///
+/// # Examples
+///
+/// ```
+/// use fibonacci::fibonacci_dp;
+/// let result = fibonacci_dp(4);
+/// assert_eq!(result, 3);
+//
+/// ```
 pub fn fibonacci_dp(n: u32) -> u32 {
     if n == 0 {
         return 0;
@@ -31,6 +80,24 @@ pub fn fibonacci_dp(n: u32) -> u32 {
 }
 
 
+///
+///
+/// # Arguments
+///
+/// * `n`:  u32, the nth number in the fibonacci sequence
+/// * `memo`:  &mut HashMap<u32, u32>, a mutable reference to a hashmap
+///
+/// returns: u32  the result of the nth number in the fibonacci sequence
+///
+/// # Examples
+///
+/// ```
+/// use fibonacci::fibonacci_memo;
+/// use std::collections::HashMap;
+/// let mut memo = HashMap::new();
+/// let result = fibonacci_memo(4, &mut memo);
+/// assert_eq!(result, 3);
+/// ```
 pub fn fibonacci_memo(n: u32, memo: &mut HashMap<u32, u32>) -> u32 {
     if let Some(&result) = memo.get(&n) {
         return result;
@@ -44,6 +111,22 @@ pub fn fibonacci_memo(n: u32, memo: &mut HashMap<u32, u32>) -> u32 {
     result
 }
 
+///
+///
+/// # Arguments
+///
+/// * `n`:  u32, the nth number in the fibonacci sequence
+///
+/// returns: u32  the result of the nth number in the fibonacci sequence
+///
+/// # Examples
+///
+/// ```
+/// use fibonacci::fibonacci_iterative;
+/// let result = fibonacci_iterative(4);
+/// assert_eq!(result, 3);
+///
+/// ```
 pub fn fibonacci_iterative(n: u32) -> u32 {
     if n == 0 {
         return 0;
--- src/measure_time.rs
+++ src/measure_time.rs
@@ -1,5 +1,24 @@
 use std::time::Instant;
 
+///
+///
+/// # Arguments
+///
+/// * `func`:  F a function that takes no arguments and returns R
+/// * `func_name`:  &str, the name of the function
+///
+/// returns: R  the result of the function
+///
+/// # Examples
+///
+/// ```
+/// use fibonacci::measure_time::measure_time;
+/// use std::collections::HashMap;
+/// use fibonacci::fibonacci_match;
+/// let mut memo: HashMap<u32, u32> = HashMap::new();
+/// let result = measure_time(|| fibonacci_match(3), "fibonacci_match");
+/// assert_eq!(result, 2);
+/// ```
 pub fn measure_time<F, R>(func: F, func_name: &str) -> R
 where
     F: FnOnce() -> R,
