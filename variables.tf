47ffa24b5aea1e10d057086c9ff27baecb08a184
Paraskevas Paraskevas <paraskevasp@gmail.com>
2025-01-08 20:05:46 +0000

    add variables in TF

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "docker_username" {
+  description = "Docker username for pulling images"
+  type        = string
+}
+
+variable "docker_image_tag" {
+  description = "Docker image tag for pulling images"
+  type        = string
+}
